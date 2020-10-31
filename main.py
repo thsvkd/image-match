72d9ca03c6b2f1fa0967eb02ec2163fbd463a140
thsvkd <thsxogud1123@gmail.com>
2020-10-31 21:48:15 +0900

    basic image-matching complete

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-a = 1
+from image_match.goldberg import ImageSignature
+
+gis = ImageSignature()
+img = []
+
+for i in range(1, 10):
+    img.append(gis.generate_signature('C:/Users/thsxo/Downloads/IMG_1987.JPG'))
+    print("%d fin" % i)
+
+distance = gis.normalized_distance(img[0], img[1])
+print(distance)
